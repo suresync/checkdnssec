b20dacf65587d465f32441fce637f1ffd1bd5f5d
Johan Bloemberg <github@ijohan.nl>
2017-10-22 17:33:15 +0200

    Auto version from git.

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import find_packages, setup
+from subprocess import check_output
+
+
+def version_from_git():
+    """Acquire package version form current git tag."""
+    return check_output(['git', 'describe', '--tags', '--abbrev=0'],
+                        universal_newlines=True)
+
 
 setup(
     name='checkdnssec',
-    version='0.1',
+    version=version_from_git(),
     packages=find_packages(),
     install_requires=[
       'dnspython',
